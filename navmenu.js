63522ecf4479174fc6cb505a005f05b456b11302
SaltmineMP4 <erhen@protonmail.ch>
2022-10-02 19:59:52 +0200

    yis

--- navmenu.js
+++ navmenu.js
@@ -1,4 +1,5 @@
-function navmenfunc() {
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
 	var x = document.getElementById("navmenu");
 	if (x.className === "PageMenu") {
 	  x.className += " responsive";
