cde41377b19612136c4a95223bd37bb8f094a7c9
SaltmineMP4 <erhen@protonmail.ch>
2022-10-03 15:26:07 +0200

    yis

--- navmenu.js
+++ navmenu.js
@@ -7,7 +7,7 @@ function navbardropdown() {
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(e) {
 	if (!e.target.matches('.dropbtn')) {
-	var dropdownMenu = document.getElementById("");
+	var dropdownMenu = document.getElementById("dropdownMenu");
 	  if (dropdownMenu.classList.contains('show')) {
 		dropdownMenu.classList.remove('show');
 	  }
