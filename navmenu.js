7b1eec805bac55cf1e19b27f70f0ff621a280db9
SaltmineMP4 <112489634+SaltmineMP4@users.noreply.github.com>
2022-10-03 13:54:35 +0200

    asdbcfg

--- navmenu.js
+++ navmenu.js
@@ -1,9 +1,24 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function addresponsiveclass() {
 	var test = document.getElementById("navmenu");
 	if (test.className === "PageMenu") {
 	  test.className += " responsive";
 	} else {
 	  test.className = "PageMenu";
+	}
+  }
+  /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function navbardropdown() {
+	document.getElementById("dropdownMenu").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+	if (!e.target.matches('.dropbtn')) {
+	var dropdownMenu = document.getElementById("dropdownMenu");
+	  if (dropdownMenu.classList.contains('show')) {
+		dropdownMenu.classList.remove('show');
+	  }
 	}
   }
