ef04bb2b9ebf16175e78450aa1e866d0d79e3d8d
SaltmineMP4 <erhen@protonmail.ch>
2022-10-03 14:56:58 +0200

    qwe

--- navmenu.js
+++ navmenu.js
@@ -1,22 +1,13 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function addresponsiveclass() {
-	var test = document.getElementById("navmenu");
-	if (test.className === "PageMenu") {
-	  test.className += " responsive";
-	} else {
-	  test.className = "PageMenu";
-	}
-  }
+
   /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function navbardropdown() {
 	document.getElementById("dropdownMenu").classList.toggle("show");
   }
-  
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(e) {
 	if (!e.target.matches('.dropbtn')) {
-	var dropdownMenu = document.getElementById("dropdownMenu");
+	var dropdownMenu = document.getElementById("");
 	  if (dropdownMenu.classList.contains('show')) {
 		dropdownMenu.classList.remove('show');
 	  }
